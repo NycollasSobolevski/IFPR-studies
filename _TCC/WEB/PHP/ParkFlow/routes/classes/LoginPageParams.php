e253bef5395deea93b1d9cf0a36fe3814eaa5255
nycollassobolevski <sobolevski.academy@gmail.com>
2024-12-20 11:41:54 -0300

    feat login

---
+++ _TCC/WEB/PHP/ParkFlow/routes/classes/LoginPageParams.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Routes\RouteParams;
+
+class LoginPageParams 
+{
+    public string $email;
+    public string $password;
+}
+
+
+?>
--- _TCC/WEB/PHP/ParkFlow/routes/web.php
+++ _TCC/WEB/PHP/ParkFlow/routes/web.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Routes\RouteParams;
+
 Route::get('/', function () {
     checkLoginOrRedirect();
     return view('welcome');
 });
 
 Route::get('/login', function () {
+
+    
+
     return view('login');
 });
